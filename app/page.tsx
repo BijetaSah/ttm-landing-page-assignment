dc49fdd41385921be8480efd7ed283f6889146f7
BijetaSah <bijetasah@30gmail.com>
2025-11-09 15:52:30 +0530

    feat: setup base page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import Benefits from "./components/Benefits";
+import Contact from "./components/Contact";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 function Page() {
-  return <div>Page</div>;
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <Benefits />
+      <Features />
+      <Contact />
+      <Footer />
+    </main>
+  );
 }
 
 export default Page;
